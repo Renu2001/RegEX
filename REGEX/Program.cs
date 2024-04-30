7145f017a5a8b6e6f23ca6ea4afed8769615ea16
Renuka <renu.bagave1901@gmail.com>
2024-04-30 11:54:09 +0530

    [Renuka]Add : Added method to validate first name of user

--- REGEX/Program.cs
+++ REGEX/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace REGEX
+﻿using System.Text.RegularExpressions;
+
+namespace REGEX
 {
     internal class Program
     {
+
+        public bool ValidName()
+        {
+            Console.WriteLine("Enter FirstName");
+            string firstname = Console.ReadLine();
+            string pattern = "^[A-Z][a-z]{2,}";
+            Regex r = new Regex(pattern);
+            if (r.IsMatch(firstname))
+                return true;
+            else
+                return false;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("User Registration UC");
-
+            Program program = new Program();
+            Console.Write(program.ValidName());
+            
         }
     }
 }
--- REGEX/obj/Debug/net7.0/REGEX.AssemblyInfo.cs
+++ REGEX/obj/Debug/net7.0/REGEX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("REGEX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfbbc47d693c6424dfeb83e6652103db964ec7f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("REGEX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("REGEX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
